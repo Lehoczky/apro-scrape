e7f5e07147d9c6dfd790fb97d711a2fdd8badfb7
Lehoczky Zoltán <zoltan.lehoczky@aimotive.com>
2020-09-11 22:53:01 +0200

    migrate project from other repo

--- src/index.js
+++ src/index.js
@@ -1,40 +1,115 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Tray,
+  Menu,
+  Notification,
+} = require("electron");
+const path = require("path");
+
+const { createScraper } = require("./scrape");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+let mainWindow = null;
+let tray = null;
+const iconPath = path.join(__dirname, "..", "static", "icon.png");
+const scrape = createScraper();
+
+const createTray = () => {
+  tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show",
+      click: () => {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Quit",
+      click: () => {
+        mainWindow.destroy();
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
+
+  tray.on("click", () => {
+    mainWindow.show();
+  });
+};
+
+const showNewItemsNotification = (items) => {
+  const notification = new Notification();
+  notification.on("click", () => {
+    mainWindow.show();
+  });
+
+  if (items.length == 1) {
+    const item = items[0];
+    notification.title = "New item is available";
+    notification.body = `${item.title} - ${item.price}`;
+  } else if (items.length > 1) {
+    notification.title = "New items are available";
+    notification.body = `${items.length} new items found!`;
+  }
+  notification.show();
+};
+
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: iconPath,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.on("minimize", () => {
+    mainWindow.hide();
+  });
 };
+
+ipcMain.on("start-scraping", (event, page) => {
+  startInterval(60, async () => {
+    const selling = await scrape(page);
+    mainWindow.webContents.send("new-items", selling);
+    if (selling.length && !mainWindow.isVisible()) {
+      showNewItemsNotification(selling);
+    }
+  });
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", () => {
+  createWindow();
+  createTray();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -42,5 +117,7 @@ app.on('activate', () => {
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+const startInterval = (seconds, callback) => {
+  callback();
+  return setInterval(callback, seconds * 1000);
+};
---
+++ src/scrape.js
@@ -0,0 +1,34 @@
+const { JSDOM } = require("jsdom");
+
+exports.createScraper = () => {
+  let lastItem = null;
+  return async (page) => {
+    let items = await getSellingItems(page);
+    const urls = items.map((item) => item.url);
+
+    if (lastItem && urls.includes(lastItem.url)) {
+      items = items.slice(0, urls.indexOf(lastItem.url));
+    }
+    if (items.length) {
+      lastItem = items[0];
+    }
+    return items;
+  };
+};
+
+// "https://hardverapro.hu/aprok/mobil/android/oneplus_keszulekek/keres.php?stext=6t&stcid=0&selling=1"
+
+const getSellingItems = async (page) => {
+  const dom = await JSDOM.fromURL(page);
+  return Array.from(dom.window.document.querySelectorAll(".media"))
+    .map((item) => {
+      const url = item.querySelector("h1 > a").href;
+      const title = item.querySelector("h1 > a").textContent;
+      const price = item.querySelector(".uad-price").textContent;
+      const location = item.querySelector(".uad-light").textContent;
+      const updated = item.querySelector(".uad-ultralight").textContent;
+      return { url, title, price, location, updated };
+    })
+    .filter((item) => item.updated != "Előresorolt hirdetés")
+    .filter((item) => item.price != "Csere");
+};
