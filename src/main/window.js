dca1b5ecc5052fa9f825a10176ec296ac61fe10d
Lehoczky <ifjlehoczkyzoltan@gmail.com>
2020-12-26 16:42:34 +0100

    update dependencies

--- src/main/window.js
+++ src/main/window.js
@@ -20,6 +20,7 @@ export const createWindow = () => {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      contextIsolation: false,
       enableRemoteModule: true,
     },
   });
