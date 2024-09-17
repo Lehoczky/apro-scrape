61426e966e9c010645eeb932b4a0de06098e9988
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-09-17 22:00:27 +0200

    fix mysterious null error

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -1,6 +1,10 @@
 import { logError } from "./errorHandling"
 
 export async function canReach(url: string) {
+  if (!url) {
+    return false
+  }
+
   try {
     const response = await fetch(url, { method: "OPTIONS" })
     return response.ok
