0a4a2574e7d9aed156288031f0499f33b69a2420
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-09-17 22:02:55 +0200

    remove eslint-define-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,8 @@
-// @ts-check
-const { defineConfig } = require("eslint-define-config")
-
-module.exports = defineConfig({
+module.exports = {
   root: true,
   parserOptions: {
     project: ["tsconfig.node.json", "tsconfig.web.json"],
   },
   extends: ["@lehoczky/eslint-config-vue"],
   ignorePatterns: ["out"],
-})
+}
